f414a263204ab749766a6c6d7d506dab88be21ff
khanh <anhkietnguyenkhanh@icloud.com>
2026-01-25 18:55:13 -0500

    Initial Commit

---
+++ Programmatic-UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Programmatic-UI
+//
+//  Created by Khanh Anh Kiet Nguyen on 2026-01-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
